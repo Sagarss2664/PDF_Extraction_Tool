cdbf81d7c8159561601e3a26476b4a114723ff4f
Sagarss2664 <01fe22bcs259@kletech.ac.in>
2025-10-22 11:49:09 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -44,7 +44,7 @@
 // export default api
 import axios from 'axios'
 
-const API_BASE_URL = 'https://pdf-extraction-tool-1.onrender.com'
+const API_BASE_URL = 'https://pdf-extraction-tool-backend.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
